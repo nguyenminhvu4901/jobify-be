5c9a3979b766e58f9f0818a95005e01bc5f45c22
Vu NM <90379036+nguyenminhvu4901@users.noreply.github.com>
2024-12-18 15:37:11 +0700

    change column name current user to current role

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'phone_number',
         'status_id',
-        'current_user',
+        'current_role',
         'avatar'
     ];
 
