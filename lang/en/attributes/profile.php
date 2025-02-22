546dd823af8f954b3a3856842594bfb331826cbb
Vu NM <90379036+nguyenminhvu4901@users.noreply.github.com>
2025-02-22 21:53:07 +0700

    msg

--- lang/en/attributes/profile.php
+++ lang/en/attributes/profile.php
@@ -33,6 +33,12 @@ return [
     'user_prize_id' => 'Prize ID',
     'category' => 'Category',
     'finished_date' => 'Finished date',
-    'user_product_id' => 'Product ID'
+    'user_product_id' => 'Product ID',
+    'attachments.*.user_product_resource_id' => 'Product Resource',
+    'attachments.*.user_course_resource_id' => 'Course Resource',
+    'attachments.*.user_prize_resource_id' => 'Prize Resource',
+    'attachments.*.user_experience_resource_id' => 'Experience Resource',
+    'attachments.*.user_certification_resource_id' => 'Certification Resource',
+    'attachments.*.user_project_resource_id' => 'Project Resource',
 ];
 
