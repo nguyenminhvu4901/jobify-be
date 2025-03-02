9ce201c58896c597edde2e74f56abba9a9a05f3e
Vu NM <90379036+nguyenminhvu4901@users.noreply.github.com>
2025-03-02 15:00:09 +0700

    Update GetListActivityCurrentUserHandle.php

--- app/Commands/UserActivity/GetListActivityCurrentUser/GetListActivityCurrentUserHandle.php
+++ app/Commands/UserActivity/GetListActivityCurrentUser/GetListActivityCurrentUserHandle.php
@@ -2,8 +2,11 @@
 
 namespace App\Commands\UserActivity\GetListActivityCurrentUser;
 
+use App\Enums\CacheTTL;
+use App\Enums\RouteNames\Profile\UserActivity;
 use App\Http\Resources\UserActivity\CurrentUserActivityResource;
 use App\Repositories\User\UserRepository;
+use Illuminate\Support\Facades\Cache;
 
 class GetListActivityCurrentUserHandle
 {
@@ -22,15 +25,26 @@ class GetListActivityCurrentUserHandle
     public function handle(): array
     {
         try {
-            $userActivities = $this->userRepository->findWithRelationships(
-                auth()->user()->id,
-                'userActivities.userActivityResources.contentType',
-                [
-                    'userProducts' => function ($query) {
-                        return $query->orderByDesc('id');
-                    }
-                ]
-            );
+            $cache = Cache::tags([UserActivity::TAG_NAME->value])->has(
+                UserActivity::LIST_ACTIVITY_CURRENT_USER->value . auth()->user()->id);
+
+            $userActivities = Cache::tags([UserActivity::TAG_NAME->value])
+                ->remember(
+                    UserActivity::LIST_ACTIVITY_CURRENT_USER->value . auth()->user()->id,
+                    CacheTTL::REMEMBER->value,
+                    function() {
+
+                        return $this->userRepository->findWithRelationships(
+                            auth()->user()->id,
+                            'userActivities.userActivityResources.contentType',
+                            [
+                                'userProducts' => function ($query) {
+                                    return $query->orderByDesc('id');
+                                }
+                            ]
+                        );
+            });
+
 
             if(empty($userActivities)){
                 return [
@@ -40,9 +54,11 @@ class GetListActivityCurrentUserHandle
 
             return [
                 'data' => CurrentUserActivityResource::make($userActivities),
-                'message' => __('messages.profile.user_get_profile_success')
+                'message' => __('messages.profile.user_get_profile_success'),
+                'cache' => $cache
             ];
         }catch (\Exception $e){
+
             return [
                 'message' => __('messages.profile.user_get_profile_error'),
                 'error' => $e
