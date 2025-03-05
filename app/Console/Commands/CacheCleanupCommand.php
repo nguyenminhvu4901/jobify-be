f89b0cb53cbe58230e955cdea5a714caa353b48a
Vu NM <90379036+nguyenminhvu4901@users.noreply.github.com>
2025-03-05 23:13:57 +0700

    using supervisor to run queue work clear cache

---
+++ app/Console/Commands/CacheCleanupCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+
+class CacheCleanupCommand extends Command
+{
+    protected $signature = 'cache:clear-redis';
+    protected $description = 'Delete all Redis cache if keys exceed 1,000,000.';
+
+    /**
+     * @return void
+     */
+    public function handle(): void
+    {
+        $redis = Redis::connection('cache');
+        $totalKeys = $redis->dbsize();
+
+        if ($totalKeys >= 1000000) {
+
+            Cache::flush();
+            Log::info("Deleted all cache because Redis had $totalKeys keys.");
+        } else {
+            Log::warning("Redis cache is below the limit, no need to delete.");
+        }
+    }
+}
