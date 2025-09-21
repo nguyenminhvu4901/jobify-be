025b65bb9c67284533951129f30a69241631f826
ZuMinhh <nguyenminhvu4901@gmail.com>
2025-09-21 18:03:29 +0700

    library octane

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,6 +6,7 @@ return [
     App\Providers\HorizonServiceProvider::class,
     App\Providers\MacroServiceProvider::class,
     App\Providers\ObserverServiceProvider::class,
+    App\Providers\OctaneServiceProvider::class,
     App\Providers\RateLimitServiceProvider::class,
     App\Providers\RelationshipServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
